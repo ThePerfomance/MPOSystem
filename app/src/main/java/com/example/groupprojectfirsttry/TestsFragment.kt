216d744242f8109efa219e3e3f0143a49be3f0a3
Nikita <128956695+ThePerfomance@users.noreply.github.com>
2025-03-05 15:41:00 +0500

    Добавил фрагмент тестов и обработчик нажатия в учебнике, чтобы он открывался

---
+++ app/src/main/java/com/example/groupprojectfirsttry/TestsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.groupprojectfirsttry
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TestsFragment:Fragment(R.layout.fragment_tests) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_tests, container, false)
+
+        return view
+    }
+}
