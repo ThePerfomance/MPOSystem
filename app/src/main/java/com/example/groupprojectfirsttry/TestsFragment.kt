cb7d9e31031a4d8083db2e1de4b812850686a5f9
Nikita <128956695+ThePerfomance@users.noreply.github.com>
2025-03-06 08:35:47 +0500

    Добавил перекраску в серый градиент и обратно

--- app/src/main/java/com/example/groupprojectfirsttry/TestsFragment.kt
+++ app/src/main/java/com/example/groupprojectfirsttry/TestsFragment.kt
@@ -4,16 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TestsFragment:Fragment(R.layout.fragment_tests) {
+
+    private lateinit var clUpHead: ConstraintLayout
+    private lateinit var bnmDown: BottomNavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_tests, container, false)
-
+        clUpHead = requireActivity().findViewById(R.id.constraintLayoutUpHead) // Ищем элемент внутри текущего фрагмента
+        bnmDown=requireActivity().findViewById(R.id.bottom_nav)
+        clUpHead.background = ResourcesCompat.getDrawable(resources, R.drawable.gradient_gray_background, context?.theme) // Устанавливаем фон
+        bnmDown.background = ResourcesCompat.getDrawable(resources, R.drawable.gradient_gray_background, context?.theme)
         return view
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        clUpHead?.background = ResourcesCompat.getDrawable(resources, R.drawable.gradient_background, context?.theme)
+        bnmDown?.background = ResourcesCompat.getDrawable(resources, R.drawable.gradient_background, context?.theme)
     }
 }
