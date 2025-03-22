6a8251c134008a73505ba8f0ee5c4947a887d63a
Nikita <128956695+ThePerfomance@users.noreply.github.com>
2025-03-22 17:33:16 +0500

    add divider and test results

--- app/src/main/java/com/example/groupprojectfirsttry/Answer.kt
+++ app/src/main/java/com/example/groupprojectfirsttry/Answer.kt
@@ -1,9 +1,42 @@
 package com.example.groupprojectfirsttry
 
-// Модель ответа
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Answer(
     val id: Int,
-    val question_id: Int,
+    val questionId: Int,
     val text: String,
-    val is_correct: Boolean
-)
+    val is_correct: Boolean,
+    var isSelected: Boolean = false
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readByte() != 0.toByte(),
+        parcel.readByte() != 0.toByte()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeInt(questionId)
+        parcel.writeString(text)
+        parcel.writeByte(if (is_correct) 1 else 0)
+        parcel.writeByte(if (isSelected) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Answer> {
+        override fun createFromParcel(parcel: Parcel): Answer {
+            return Answer(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Answer?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
